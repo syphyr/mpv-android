2668652c80b43802ec572bbc82cdbb87ae43e707
sfan5 <sfan5@live.de>
2022-04-07 17:43:31 +0200

    python build script

--- buildscripts/include/depinfo.sh
+++ buildscripts/include/depinfo.sh
@@ -15,6 +15,8 @@ v_harfbuzz=10.1.0
 v_fribidi=1.0.16
 v_freetype=2-13-3
 v_mbedtls=3.6.2
+v_openssl=3.0.15
+v_python=3.9.21
 
 
 ## Dependency tree
@@ -31,7 +33,9 @@ dep_libass=(freetype2 fribidi harfbuzz unibreak)
 dep_lua=()
 dep_libplacebo=()
 dep_mpv=(ffmpeg libass lua libplacebo)
-dep_mpv_android=(mpv)
+dep_openssl=()
+dep_python=(openssl)
+dep_mpv_android=(mpv python)
 
 
 ## for CI workflow
