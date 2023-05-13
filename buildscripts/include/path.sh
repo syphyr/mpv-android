3382a20cc135393ffb6a2c568b43916c875dacd4
syphyr <syphyr@gmail.com>
2023-05-13 23:48:24 +0200

    Create symlink to Android SDK
    If ANDROID_SDK_ROOT env variable is already defined, create a link
    to it instead of downloading it again.

--- buildscripts/include/path.sh
+++ buildscripts/include/path.sh
@@ -29,4 +29,7 @@ toolchain=$(echo "$DIR/sdk/android-ndk-${v_ndk}/toolchains/llvm/prebuilt/"*)
 [ -d "$toolchain" ] && \
 	export PATH="$toolchain/bin:$DIR/sdk/android-ndk-${v_ndk}:$DIR/sdk/bin:$PATH"
 export ANDROID_HOME="$DIR/sdk/android-sdk-$os"
+if [ -n "${ANDROID_SDK_ROOT}" ]; then
+	export ANDROID_SDK_ROOT_OLD="$ANDROID_SDK_ROOT"
+fi
 unset ANDROID_SDK_ROOT ANDROID_NDK_ROOT
