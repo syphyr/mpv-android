7d29f87ead50cfed1f83c1e033c06bf6dc6fc62d
syphyr <syphyr@gmail.com>
2023-05-31 23:44:57 +0200

    Default to local user directory for meson install

--- buildscripts/include/download-sdk.sh
+++ buildscripts/include/download-sdk.sh
@@ -12,7 +12,7 @@ if [ "$os" == "linux" ]; then
 		if hash yum &>/dev/null; then
 			sudo yum install autoconf pkgconfig libtool ninja-build \
 				unzip wget
-			python3 -m pip install --upgrade meson
+			python3 -m pip install --upgrade --user meson
 		elif apt-get -v &>/dev/null; then
 			dpkg -l autoconf | grep "no description" &>/dev/null && { sudo apt-get install autoconf; }
 			dpkg -l pkg-config | grep "no description" &>/dev/null && { sudo apt-get install pkg-config; }
@@ -20,7 +20,7 @@ if [ "$os" == "linux" ]; then
 			dpkg -l ninja-build | grep "no description" &>/dev/null && { sudo apt-get install ninja-build; }
 			dpkg -l unzip | grep "no description" &>/dev/null && { sudo apt-get install unzip; }
 			dpkg -l wget | grep "no description" &>/dev/null && { sudo apt-get install wget; }
-			python3 -m pip show meson | grep WARNING &>/dev/null && { python3 -m pip install --upgrade meson; }
+			python3 -m pip show meson | grep WARNING &>/dev/null && { python3 -m pip install --upgrade --user meson; }
 		else
 			echo "Note: dependencies were not installed, you have to do that manually."
 		fi
