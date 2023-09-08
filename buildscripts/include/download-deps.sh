03044ab3d2a0cc770e8eaff99fc320c199fc5709
syphyr <syphyr@gmail.com>
2023-09-08 23:30:10 +0200

    Update freetype when version changes

--- buildscripts/include/download-deps.sh
+++ buildscripts/include/download-deps.sh
@@ -30,7 +30,16 @@ if [ ! -d ffmpeg ]; then
 fi
 
 # freetype2
-[ ! -d freetype2 ] && git clone --recurse-submodules git://git.sv.nongnu.org/freetype/freetype2.git -b VER-$v_freetype
+if [ ! -d freetype2 ]; then
+	git clone --recurse-submodules git://git.sv.nongnu.org/freetype/freetype2.git -b VER-$v_freetype
+else
+	cd freetype2
+	git fetch
+	git submodule update --init --recursive --rebase
+	git checkout VER-$v_freetype
+	cd ..
+fi
+
 
 # fribidi
 if [ ! -d fribidi ]; then
