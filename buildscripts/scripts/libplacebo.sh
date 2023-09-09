8f987721269a9d96e1eeebf3407aba1edfa6e811
syphyr <syphyr@gmail.com>
2023-09-09 23:55:35 +0200

    Disable vulkan support in libmpv
    Vulkan is not supported on most devices and does not work
    correctly yet on android

--- buildscripts/scripts/libplacebo.sh
+++ buildscripts/scripts/libplacebo.sh
@@ -15,7 +15,7 @@ fi
 
 unset CC CXX
 meson setup $build --cross-file "$prefix_dir"/crossfile.txt \
-	-Dvk-proc-addr=enabled -Ddemos=false
+	-Dvulkan=disabled -Ddemos=false
 
 ninja -C $build -j$cores
 DESTDIR="$prefix_dir" ninja -C $build install
