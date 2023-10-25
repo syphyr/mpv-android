b5cea3037c2a3acc0dd92c6ad4cb7b93f8d9a57b
syphyr <syphyr@gmail.com>
2023-10-26 00:37:15 +0200

    Update renamed mpv options

--- app/src/main/java/is/xyz/mpv/MPVView.kt
+++ app/src/main/java/is/xyz/mpv/MPVView.kt
@@ -113,11 +113,11 @@ internal class MPVView(context: Context, attrs: AttributeSet) : BaseMPVView(cont
         //
         val screenshotDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES)
         screenshotDir.mkdirs()
-        MPVLib.setOptionString("screenshot-directory", screenshotDir.path)
+        MPVLib.setOptionString("screenshot-dir", screenshotDir.path)
         val watchlaterDir = File(Environment.getExternalStorageDirectory().path +
                                  "/Android/media/" + context.getPackageName() + "/watch_later")
         watchlaterDir.mkdirs()
-        MPVLib.setOptionString("watch-later-directory", watchlaterDir.toString())
+        MPVLib.setOptionString("watch-later-dir", watchlaterDir.toString())
     }
 
     override fun postInitOptions() {
