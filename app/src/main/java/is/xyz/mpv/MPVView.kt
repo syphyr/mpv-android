6ad0efebc795da608323a6fd665cffc378b424b2
syphyr <syphyr@gmail.com>
2024-01-12 22:07:06 +0100

    Force hardware plus decoding

--- app/src/main/java/is/xyz/mpv/MPVView.kt
+++ app/src/main/java/is/xyz/mpv/MPVView.kt
@@ -34,22 +34,25 @@ internal class MPVView(context: Context, attrs: AttributeSet) : SurfaceView(cont
     }
 
     private var voInUse: String = ""
+    private val sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this.context)
 
     private fun initOptions() {
-        val sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this.context)
-
         // apply phone-optimized defaults
         MPVLib.setOptionString("profile", "fast")
 
         // vo
-        val vo = if (sharedPreferences.getBoolean("gpu_next", false))
+        val vo = if (forceHardwarePlus())
+            "mediacodec_embed"
+        else if (enableGpuNext())
             "gpu-next"
         else
             "gpu"
         voInUse = vo
 
         // hwdec
-        val hwdec = if (sharedPreferences.getBoolean("hardware_decoding", true))
+        val hwdec = if (forceHardwarePlus())
+            "mediacodec"
+        else if (sharedPreferences.getBoolean("hardware_decoding", true))
             "auto"
         else
             "no"
@@ -389,7 +392,17 @@ internal class MPVView(context: Context, attrs: AttributeSet) : SurfaceView(cont
     fun cyclePause() = MPVLib.command(arrayOf("cycle", "pause"))
     fun cycleAudio() = MPVLib.command(arrayOf("cycle", "audio"))
     fun cycleSub() = MPVLib.command(arrayOf("cycle", "sub"))
-    fun cycleHwdec() = MPVLib.command(arrayOf("cycle-values", "hwdec", "auto", "no"))
+    fun cycleHwdec() {
+        if (!forceHardwarePlus())
+            MPVLib.command(arrayOf("cycle-values", "hwdec", "auto", "no"))
+        else {
+            MPVLib.command(arrayOf("cycle-values", "hwdec", "mediacodec", "mediacodec-copy"))
+            if (enableGpuNext())
+                MPVLib.command(arrayOf("cycle-values", "vo", "mediacodec_embed", "gpu-next"))
+            else
+                MPVLib.command(arrayOf("cycle-values", "vo", "mediacodec_embed", "gpu"))
+        }
+    }
 
     fun cycleSpeed() {
         val speeds = arrayOf(0.5, 0.75, 1.0, 1.25, 1.5, 1.75, 2.0)
@@ -420,6 +433,14 @@ internal class MPVView(context: Context, attrs: AttributeSet) : SurfaceView(cont
 
     fun getShuffle(): Boolean {
         return MPVLib.getPropertyBoolean("shuffle")
+    }
+
+    fun forceHardwarePlus(): Boolean {
+        return sharedPreferences.getBoolean("hardware_plus_decoding", false)
+    }
+
+    fun enableGpuNext(): Boolean {
+        return sharedPreferences.getBoolean("gpu_next", false)
     }
 
     fun changeShuffle(cycle: Boolean, value: Boolean = true) {
