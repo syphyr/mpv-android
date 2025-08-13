226d302582a50564a08f1abd724e2ad3ee29ae5d
syphyr <syphyr@gmail.com>
2025-08-13 21:12:53 +0200

    A better way to check if packages are installed

--- buildscripts/include/download-sdk.sh
+++ buildscripts/include/download-sdk.sh
@@ -14,14 +14,14 @@ if [ "$os" == "linux" ]; then
 				unzip wget meson python3
 			python3 -m pip install --upgrade --user meson
 		elif apt-get -v &>/dev/null; then
-			dpkg -l autoconf | grep "no description" &>/dev/null && { sudo apt-get install autoconf; }
-			dpkg -l pkg-config | grep "no description" &>/dev/null && { sudo apt-get install pkg-config; }
-			dpkg -l libtool | grep "no description" &>/dev/null && { sudo apt-get install libtool; }
-			dpkg -l ninja-build | grep "no description" &>/dev/null && { sudo apt-get install ninja-build; }
-			dpkg -l unzip | grep "no description" &>/dev/null && { sudo apt-get install unzip; }
-			dpkg -l wget | grep "no description" &>/dev/null && { sudo apt-get install wget; }
-			dpkg -l meson | grep "no description" &>/dev/null && { sudo apt-get install meson; }
-			dpkg -l python3 | grep "no description" &>/dev/null && { sudo apt-get install python3; }
+			dpkg -l autoconf | grep "^ii" &>/dev/null || { sudo apt-get install autoconf; }
+			dpkg -l pkg-config | grep "^ii" &>/dev/null || { sudo apt-get install pkg-config; }
+			dpkg -l libtool | grep "^ii" &>/dev/null || { sudo apt-get install libtool; }
+			dpkg -l ninja-build | grep "^ii" &>/dev/null || { sudo apt-get install ninja-build; }
+			dpkg -l unzip | grep "^ii" &>/dev/null || { sudo apt-get install unzip; }
+			dpkg -l wget | grep "^ii" &>/dev/null || { sudo apt-get install wget; }
+			dpkg -l meson | grep "^ii" &>/dev/null || { sudo apt-get install meson; }
+			dpkg -l python3 | grep "^ii" &>/dev/null || { sudo apt-get install python3; }
 			python3 -m pip show meson | grep WARNING &>/dev/null && { python3 -m pip install --upgrade --user meson; }
 		else
 			echo "Note: dependencies were not installed, you have to do that manually."
